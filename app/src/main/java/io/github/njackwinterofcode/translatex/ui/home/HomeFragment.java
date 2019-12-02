b9d4de7ffc950350614b0e138c292518b9820d8f
Ashish Arora <ashish007722@gmail.com>
2019-12-02 17:19:26 +0530

    integrated Firebase with the application home fragment can take a english String and translate it into german and show a Toast (initial implementation)
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

--- app/src/main/java/io/github/njackwinterofcode/translatex/ui/home/HomeFragment.java
+++ app/src/main/java/io/github/njackwinterofcode/translatex/ui/home/HomeFragment.java
@@ -1,28 +1,50 @@
 package io.github.njackwinterofcode.translatex.ui.home;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslator;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions;
 
 import io.github.njackwinterofcode.translatex.R;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private Button translate;
+    private TextInputLayout textToTranslate;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        translate = root.findViewById(R.id.translateButton);
+        textToTranslate = root.findViewById(R.id.texttotranslate);
+        translate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                translateX(textToTranslate.getEditText().getText().toString());
+            }
+        });
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -31,5 +53,58 @@ public class HomeFragment extends Fragment {
             }
         });
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    public void translateX(final String str) {
+        FirebaseTranslatorOptions options =
+                new FirebaseTranslatorOptions.Builder()
+                        .setSourceLanguage(FirebaseTranslateLanguage.EN)
+                        .setTargetLanguage(FirebaseTranslateLanguage.DE)
+                        .build();
+        final FirebaseTranslator englishGermanTranslator =
+                FirebaseNaturalLanguage.getInstance().getTranslator(options);
+
+        FirebaseModelDownloadConditions conditions = new FirebaseModelDownloadConditions.Builder()
+                .requireWifi()
+                .build();
+        final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setTitle("Downloading langauge Model for translation");
+        progressDialog.setMessage("Please wait while the download is in progress");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+        englishGermanTranslator.downloadModelIfNeeded(conditions)
+                .addOnSuccessListener(
+                        new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void v) {
+                                // Model downloaded successfully. Okay to start translating.
+                                // (Set a flag, unhide the translation UI, etc.)
+                                progressDialog.dismiss();
+                                englishGermanTranslator.translate(str)
+                                        .addOnSuccessListener(new OnSuccessListener<String>() {
+                                            @Override
+                                            public void onSuccess(String s) {
+                                                Toast.makeText(getActivity(), s, Toast.LENGTH_LONG).show();
+                                            }
+                                        });
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Model couldn’t be downloaded or other internal error.
+                                // ...
+                                progressDialog.hide();
+                                Toast.makeText(getActivity(), "Unable to translate", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
     }
 }
